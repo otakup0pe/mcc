e91be0b1a9dfebbd64ec0f3d85b50d2010ef6c57
Jonathan Freedman <jonafree@gmail.com>
2012-09-10 14:21:08 -0700

    remove dbg shortcuts

--- src/mcc_shell.erl
+++ src/mcc_shell.erl
@@ -2,16 +2,14 @@
 -behaviour(shellbeam).
 
 -export([commands/0]).
--export([cfglist/0, cfglist/1, cfgget/2, cfgset/3, dbgon/1, dbgoff/1]).
+-export([cfglist/0, cfglist/1, cfgget/2, cfgset/3]).
 
 commands() ->
     [
      {["list"], "List configuration namespaces", fun cfglist/0},
      {["list", {"namespace", atom}], "List configuration items in a namespace", fun cfglist/1},
      {["get", {"namespace", atom}, {"key", atom}], "Retrieve a configuration item", fun cfgget/2},
-     {["set", {"namespace", atom}, {"key", atom}, {"value", string}], "Set a configuration item", fun cfgset/3},
-     {["dbgon", {"tag", atom}], "Enable a debug tag", fun dbgon/1},
-     {["dbgoff", {"tag", atom}], "Disable a debug tag", fun dbgoff/1}
+     {["set", {"namespace", atom}, {"key", atom}, {"value", string}], "Set a configuration item", fun cfgset/3}
     ].
 
 cfglist() ->
@@ -30,12 +28,3 @@ cfgget(Name, Key) ->
 cfgset(Name, Key, Value) ->
     ok = mcc:set(Name, Key, Value),
         {ok, "Set ~p:~p : ~p", [Name, Key, Value]}.
-
-dbgon(Name) ->
-    n(Name, true),
-        {ok, "Tag ~p set", [Name]}.
-
-dbgoff(Name) ->
-    
-    nomura_config:debug_set(Name, false),
-        {ok, "Tag ~p unset", [Name]}.
